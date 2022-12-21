feeec98a31f39d1e165986499e35245f566c9e31
ims-idlenara <gudwnsrh@i-nara.co.kr>
2022-12-21 19:36:54 +0900

    feat: [NO-ISSUE] 초기 프로젝트 세팅

---
+++ src/main/java/com/example/upview/UpviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.upview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/upview/UpviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
