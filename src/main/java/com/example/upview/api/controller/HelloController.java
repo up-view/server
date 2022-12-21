0cc16c4bd302f00c42242a25917914fcd6919841
ims-idlenara <gudwnsrh@i-nara.co.kr>
2022-12-21 20:10:19 +0900

    feat: test 기능추가

--- src/main/java/com/example/upview/api/controller/HelloController.java
+++ src/main/java/com/example/upview/api/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String helloWorld(){
         return "Hello World!";
     }
+
+    @GetMapping
+    public String test(){
+        return "test";
+    }
 }
